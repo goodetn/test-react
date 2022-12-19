5a2ed79fae989a396603e7a1a825f3f5f8f6b0d3
tottran <tottianddona@gmail.com>
2022-12-19 09:18:16 +0700

    🎉 First test

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import "./App.scss";
 
 function App() {
-  return (
-    <div className="App-header">
-    </div>
-  );
+  return <div className="App-header">learn test</div>;
 }
 
 export default App;
