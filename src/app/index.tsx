bdd57681a403430b1a6e8f6226319534fd6d7edb
tottran <tottianddona@gmail.com>
2022-12-19 16:53:46 +0700

    Update app content page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import "./index.scss";
-
-function App() {
-  return <div className="App-header">learn test</div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React - learn test</h1>
+          <nav className="navbar" role="navigation">
+            <ul>
+              <li className="nal-li">
+                <a href="/#">Batman</a>
+              </li>
+              <li className="nal-li">
+                <a href="/#">Supermman</a>
+              </li>
+              <li className="nal-li">
+                <a href="/#">Aquaman</a>
+              </li>
+              <li className="nal-li">
+                <a href="/#">Wonder Woman</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
-
 export default App;
