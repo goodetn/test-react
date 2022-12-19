645169ef579cb13d14d5eed0f0c9419ae2e97ddc
tottran <tottianddona@gmail.com>
2022-12-19 10:50:50 +0700

    Add introduce Mock test

---
+++ src/components/Mock/index.test.tsx
@@ -0,0 +1,21 @@
+describe("mock", () => {
+  it("TODO", () => {});
+});
+
+/**
+ * đặt vấn đề:
+ * - test những hàm tính toán gọi từ api, db
+ *
+ * nhược điểm:
+ *
+ * giải pháp:
+ * - phải tách hàm ra thành các module và phải mock
+ *  (bonus: mock là làm những module giả, thay vì gọi api, db thì nó sẽ lấy dữ liệu do mình cài đặt trước giúp test dễ hơn.)
+ *
+ * phần mềm:
+ *
+ * ưu điểm:
+ *
+ * cú pháp:
+ *
+ */
